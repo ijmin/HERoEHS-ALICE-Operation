22ef5942002560368f5185c63ced34b2e6d8c699
robotemperor <leeche12@hanyang.ac.kr>
2018-04-26 21:24:50 +0900

    m

--- alice_foot_step_planner/include/alice_foot_step_planner/alice_foot_step_planner.h
+++ alice_foot_step_planner/include/alice_foot_step_planner/alice_foot_step_planner.h
@@ -13,7 +13,10 @@
 #include "robotis_controller_msgs/StatusMsg.h"
 #include "robotis_math/robotis_math.h"
 #include "alice_foot_step_generator/FootStepCommand.h"
+
+#include <math.h>
 #include <cmath>
+#include <stdio.h>
 
 
 namespace alice
@@ -37,8 +40,13 @@ public:
 
 private:
 
+	double step_length_max;
+	double step_length_min;
+
+
 	void AlignRobotYaw(double yaw_degree);
-	void CalculateStepData(double x, double pre_x, double y, double pre_y);
+	void CalculateStepData(double x, double pre_x, double y, double pre_y, double r_point_x, double r_point_y);
+	Eigen::Matrix4d TransfomationGoalPointOnRobot(double robot_x, double robot_y, double robot_yaw_degree, double g_point_x, double g_point_y);
 	bool CheckArrival(std::string status, int via_point_num, int all_point_num);
 };
 
--- alice_foot_step_planner/src/alice_foot_step_planner.cpp
+++ alice_foot_step_planner/src/alice_foot_step_planner.cpp
@@ -12,6 +12,10 @@ using namespace alice;
 FootStepPlanner::FootStepPlanner()
 {
 
+	// load yaml
+	step_length_max = 0;
+	step_length_min = 0;
+
 }
 FootStepPlanner::~FootStepPlanner()
 {
@@ -45,10 +49,33 @@ void FootStepPlanner::initialize()
 	foot_step_command_pub     = nh.advertise<alice_foot_step_generator::FootStepCommand>("/heroehs/alice_foot_step_planner/walking_command", 1);
 }
 
+Eigen::Matrix4d FootStepPlanner::TransfomationGoalPointOnRobot(double robot_x, double robot_y, double robot_yaw_degree, double g_point_x, double g_point_y)
+{
+	Eigen::Matrix4d g_T_robot;
+	Eigen::Matrix4d g_T_point;
+	Eigen::Matrix4d robot_T_g;
+	Eigen::Matrix4d robot_T_point;
+
+	g_T_robot.fill(0);
+	g_T_point.fill(0);
+	robot_T_g.fill(0);
+	robot_T_point.fill(0);
+
+	g_T_point <<
+			1, 0, 0, g_point_x,
+			0, 1, 0, g_point_y,
+			0, 0, 1, 0,
+			0, 0, 0, 1;
+
+	g_T_robot = robotis_framework::getTransformationXYZRPY(robot_x,robot_y,0,0,0,robot_yaw_degree*DEGREE2RADIAN);
+	robot_T_g = robotis_framework::getInverseTransformation(g_T_robot);
+	robot_T_point = robot_T_g * g_T_point;
+	return robot_T_point;
+}
+
 void FootStepPlanner::AlignRobotYaw(double yaw_degree)
 {
 	double yaw_rad = yaw_degree*DEGREE2RADIAN;
-
 	if(yaw_rad > 0) // turn left
 	{
 		foot_set_command_msg.step_angle_rad = yaw_rad;
@@ -63,11 +90,44 @@ void FootStepPlanner::AlignRobotYaw(double yaw_degree)
 	{
 		return;
 	}
-
 	foot_step_command_pub.publish(foot_set_command_msg);
 }
-void FootStepPlanner::CalculateStepData(double x, double pre_x, double y, double pre_y)
+void FootStepPlanner::CalculateStepData(double x, double pre_x, double y, double pre_y, double r_point_x, double r_point_y)
 {
+	int step_num_temp_ = 0;
+	double step_length_temp_ = 0;
+	double desired_distance_ = 0;
+
+	desired_distance_ = sqrt(pow((x-pre_x),2) + pow((y-pre_y),2));
+
+	if((pre_x - x) == 0 || (pre_y - y) == 0) // 게걸음 나중에.
+	{
+
+	}
+	else
+	{
+		for(int num = 1; num < (desired_distance_/step_length_max)+1 ; num++)// 최대 걸음수 계산
+		{
+			if((step_length_min < desired_distance_/num) && ((step_length_max > desired_distance_/num))) // 로봇이 갈수 있는 (뻗을수있는 length 범위 안에 들어온다면,)
+			{
+				step_length_temp_ = desired_distance_/num;
+				step_num_temp_ = num;
+				foot_set_command_msg.step_num = step_num_temp_;
+				foot_set_command_msg.step_length = step_length_temp_;
+				printf("step_length_temp :: %f  step_num_temp :: %d  \n", step_length_temp_, num);
+				break;
+			}
+		}
+		if(r_point_y > 0)
+			foot_set_command_msg.command = "forward";
+		else if (r_point_y < 0)
+			foot_set_command_msg.command = "backward";
+		else
+			foot_set_command_msg.command = "stop";
+
+		foot_set_command_msg.step_time = 2; //변경 할 수 있음
+		foot_step_command_pub.publish(foot_set_command_msg);
+	}
 
 }
 bool FootStepPlanner::CheckArrival(std::string status, int via_point_num, int all_point_num)
