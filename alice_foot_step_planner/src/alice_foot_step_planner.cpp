38d5ea329a24a173390dc56d5f11ea50b107f410
robotemperor <leeche12@hanyang.ac.kr>
2019-06-25 23:39:27 +0900

    modified

--- alice_foot_step_planner/src/alice_foot_step_planner.cpp
+++ alice_foot_step_planner/src/alice_foot_step_planner.cpp
@@ -270,7 +270,7 @@ void FootStepPlanner::moveCommandStatusMsgCallback(const diagnostic_msgs::KeyVal
     }
   }
 
-  if(move_command->key != "left_kick" && move_command->key != "right_kick")
+  if(move_command->key != "left_kick" && move_command->key != "right_kick" && move_command->key != "y_type_left_kick" && move_command->key != "y_type_right_kick")
   {
     if(previous_motion_check != motion_check && motion_check == false)
     {
@@ -433,7 +433,7 @@ void FootStepPlanner::moveCommandStatusMsgCallback(const diagnostic_msgs::KeyVal
     command_controller->Set_FootParam(alice_id_int);
     foot_set_command_msg = command_controller->FootParam;
 
-    if ((move_command->key == "left_kick" || move_command->key == "right_kick" )&& walking_check == true)
+    if ((move_command->key == "left_kick" || move_command->key == "right_kick" || move_command->key == "y_type_left_kick" || move_command->key == " y_type_right_kick")&& walking_check == true)
     {
       command_interval_check = 0;
       return;
@@ -456,6 +456,32 @@ void FootStepPlanner::moveCommandStatusMsgCallback(const diagnostic_msgs::KeyVal
       //walking_check = true;
       change_walking_kick_mode("kick", "right kick");
       foot_set_command_msg.command = "right kick";
+      foot_step_command_pub.publish(foot_set_command_msg);
+      motion_check = true; // motion start
+      ///////////////////////////////////////
+      previous_motion_check = motion_check;
+      command_interval_check = 0;
+      ///////////////////////////////////////
+      return;
+    }
+    if (move_command->key == "y_type_left_kick" && walking_check == false) //kick
+    {
+      //walking_check = true;
+      change_walking_kick_mode("kick", "left kick");
+      foot_set_command_msg.command = "y type left kick";
+      foot_step_command_pub.publish(foot_set_command_msg);
+      motion_check = true; // motion start
+      ///////////////////////////////////////
+      previous_motion_check = motion_check;
+      command_interval_check = 0;
+      ///////////////////////////////////////
+      return;
+    }
+    if(move_command->key == "y_type_right_kick" && walking_check == false)
+    {
+      //walking_check = true;
+      change_walking_kick_mode("kick", "right kick");
+      foot_set_command_msg.command = "y type right kick";
       foot_step_command_pub.publish(foot_set_command_msg);
       motion_check = true; // motion start
       ///////////////////////////////////////
